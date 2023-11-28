6af21ded549b7e5b6b9eff4806169a82c4290f2e
Lylouf13 <lylian.leutard@gmail.com>
2023-11-28 11:55:16 +0100

    integrated caroussel

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,65 @@
+const arrows = document.querySelectorAll(".arrow")
+const dotsContainer = document.querySelector(".dots")
+const carousselCurrent = document.querySelector(".banner-img")
+const dots = []
+
+let carousselIndex = 0;
+
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+for (let i=0; i<slides.length; i++){
+	let newDot = document.createElement("div")
+	newDot.classList.add("dot")
+	if(i===0){
+		newDot.classList.add("dot_selected")
+	}
+	dotsContainer.appendChild(newDot)
+	dots.push(newDot);
+}
+
+
+arrows[0].addEventListener("click", () =>{
+	dots[carousselIndex].classList.remove("dot_selected")
+
+	if(carousselIndex === 0){
+		carousselIndex=slides.length-1
+	}
+	else{	
+		carousselIndex--
+	}
+	dots[carousselIndex].classList.add("dot_selected")
+	carousselCurrent.setAttribute("src", slides[carousselIndex].image)
+
+})
+
+arrows[1].addEventListener("click", () =>{
+	dots[carousselIndex].classList.remove("dot_selected")
+	carousselIndex++
+
+	if(carousselIndex === slides.length){
+		carousselIndex = 0
+	}
+
+	dots[carousselIndex].classList.add("dot_selected")
+	carousselCurrent.setAttribute("src", slides[carousselIndex].image)
+})
+
